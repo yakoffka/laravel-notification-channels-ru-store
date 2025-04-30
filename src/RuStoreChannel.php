33f9e1fb4533798e0825e1ad4e75360159706e56
yakoffka <yagithub@mail.ru>
2025-04-30 13:41:27 +0300

    Writing some basic notification sending logic

--- src/RuStoreChannel.php
+++ src/RuStoreChannel.php
@@ -2,11 +2,16 @@
 
 namespace NotificationChannels\RuStore;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 use NotificationChannels\RuStore\Exceptions\CouldNotSendNotification;
 use Illuminate\Notifications\Notification;
 
 class RuStoreChannel
 {
+    public const MAX_PAYLOAD_LENGTH = 4078;
+
     public function __construct()
     {
         // Initialisation code here
@@ -16,16 +21,37 @@ class RuStoreChannel
      * Send the given notification.
      *
      * @param mixed $notifiable
-     * @param \Illuminate\Notifications\Notification $notification
+     * @param Notification $notification
      *
-     * @throws \NotificationChannels\RuStore\Exceptions\CouldNotSendNotification
+     * @return void
      */
-    public function send($notifiable, Notification $notification)
+    public function send($notifiable, Notification $notification): void
     {
         //$response = [a call to the api of your notification send]
+        //
+        // if ($response->error) { // replace this by the code need to check for errors
+        //     throw CouldNotSendNotification::serviceRespondedWithAnError($response);
+        // }
 
-//        if ($response->error) { // replace this by the code need to check for errors
-//            throw CouldNotSendNotification::serviceRespondedWithAnError($response);
-//        }
+        $tokens = Arr::wrap($notifiable->routeNotificationFor('ruStore', $notification));
+
+        if (empty($tokens)) {
+            return;
+        }
+
+        /** @var RuStoreMessage $message */
+        $message = $notification->toRuStore($notifiable);
+
+        $response = Collection::make($tokens)->map(function ($token) use ($message) {
+            $message->token($token);
+            $payload = json_encode(['message' => $message->toArray()], JSON_THROW_ON_ERROR);
+            $url = sprintf(config('ru-store.url'), config('ru-store.project_id'));
+
+            $pending_request = Http::withToken(config('ru-store.token'))
+                ->withBody($payload)
+                ->send('POST', $url);
+
+            // dump($pending_request);
+        });
     }
 }
