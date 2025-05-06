f245d220bdf51b80da8b246bac579d3e1a9983be
yakoffka <yagithub@mail.ru>
2025-05-06 07:31:08 +0300

    Restoring test functionality: removing dependency on .env file
    composer test
    
    Time: 00:00.068, Memory: 26.00 MB
    
    OK (5 tests, 9 assertions)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace NotificationChannels\RuStore\Test;
 
-use Dotenv\Dotenv;
 use NotificationChannels\RuStore\RuStoreServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -19,8 +18,6 @@ abstract class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-        $this->setEnv();
-        $this->setUrl();
     }
 
     /**
@@ -30,30 +27,5 @@ abstract class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [RuStoreServiceProvider::class];
-    }
-
-    /**
-     * Загрузка переменных из .env
-     *
-     * @return void
-     */
-    private function setEnv(): void
-    {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-        $dotenv->load();
-
-        $this->app['config']->set('ru-store.project_id', env('RUSTORE_PROJECT_ID'));
-        $this->app['config']->set('ru-store.token', env('RUSTORE_TOKEN'));
-    }
-
-    /**
-     * Получение url для отправки push-уведомления
-     *
-     * @return void
-     * @todo дублирование кода
-     */
-    private function setUrl()
-    {
-        $this->url = sprintf(config('ru-store.url'), config('ru-store.project_id'));
     }
 }
